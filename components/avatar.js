74dbcd4dd5a580c199e66c990340e58dcf3b53f6
Joao Vieira <joaoantoniopintovieira13@gmail.com>
2022-01-22 18:27:39 +0000

    Index Page Finished

--- components/avatar.js
+++ components/avatar.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { AiFillInstagram, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 function avatar() {
     return (
         <div className='mt-32 ml-60 flex'>
             <img src="./Joao.jpeg" alt="sou lindo" width={400} height={400} className='rounded-full' />
-            <div className='ml-16 mt-10'>
-                <p className='mb-4 font-medium'>HI THERE! I'M</p>
+            <div className='ml-16 mt-16'>
+                <p className='mb-4 font-medium font-Poppins'>HI THERE! I'M</p>
                 <div className='flex mb-4'>
-                    <p className='text-purple-900 text-4xl font-bold'>JOÃO</p>
-                    <p className='text-4xl font-bold ml-3'>VIEIRA</p>
+                    <p className='text-purple-900 text-4xl font-bold font-Ubuntu'>JOÃO</p>
+                    <p className='text-4xl font-bold ml-3 font-Ubuntu'>VIEIRA</p>
                 </div>
-                <p className='text-purple-900'>A Junior Front-End Developer <span className='text-black'>
+                <p className='text-purple-900 font-Poppins leading-7'>A Junior Front-End Developer <span className='text-black'>
                     interested to learn  <br /> new technologies and create interactive applications<br />
                     and experiencies on the web.</span></p>
-                <button className='mt-16 bg-purple-900 h-9 w-28 rounded-xl text-white'>Resume</button>
+                <div className='flex mt-12'>
+                    <button className='bg-purple-900 h-9 w-28 rounded-xl text-white'>Resume</button>
+                    <div className='flex ml-14'>
+                        <a href='https://www.instagram.com/joaopintovieira13/'><AiFillInstagram className='w-8 h-8 ml-3' /></a>
+                        <a href='https://www.linkedin.com/in/jo%C3%A3o-vieira-833579215/'><AiFillLinkedin className='w-8 h-8 ml-3' /></a>
+                        <a href='https://github.com/JoaoVieira13'><AiFillGithub className='w-8 h-8 ml-3' /></a>
+                    </div>
+                </div>
             </div>
         </div>
     );
